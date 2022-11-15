15c307e2a024f8a1aed5c4bc5c044765445cdf8f
JosephRioJacob19 <113467199+JosephRioJacob19@users.noreply.github.com>
2022-11-16 05:06:45 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 4/sprint4.py
@@ -0,0 +1,28 @@
+import json
+import wiotp.sdk.device
+import time
+
+myConfig = {
+    "identity": {
+    "orgId": "zc4u6v",
+    "typeId": "ChildSafetyGadget",
+    "deviceId": "PNT2022TMID15707"
+ 
+    },
+    "auth": {
+        "token": "childsafety@123"
+    }
+}
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+        name= "Smartbridge"
+        latitude=17.4225176
+        longitude=78.5458842
+        myData={'name': name, 'lat': latitude,'lon': longitude}
+        client.publishEvent(eventId="status",msgFormat="json", data=myData, qos=0, onPublish=None)
+        print("Data published to IBM IOT platform :",myData)
+        time.sleep(5)
+        
+client.disconnect()
